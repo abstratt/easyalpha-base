ca72854fbfcf91cc87a027de5963a5b737d0ddf8
Rafael Chaves <rafael@abstratt.com>
2018-02-27 11:08:45 -0300

    "progress"

--- easyalpha-core/src/main/java/com/abstratt/easyalpha/core/infra/EasyAlphaApplication.kt
+++ easyalpha-core/src/main/java/com/abstratt/easyalpha/core/infra/EasyAlphaApplication.kt
@@ -4,14 +4,23 @@ import org.springframework.boot.SpringApplication
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.support.PropertiesLoaderUtils
 import java.util.*
+import kotlin.collections.LinkedHashMap
 import kotlin.reflect.KClass
 
 
-abstract class EasyAlphaApplication
+abstract class EasyAlphaApplication {
+    constructor() {
+        // remember this application class
+        applications[this.javaClass.`package`.name] = this.javaClass.kotlin
+    }
+    companion object {
+        private var applications : MutableMap<String, KClass<EasyAlphaApplication>> = LinkedHashMap()
+    }
+}
 
 fun <C : EasyAlphaApplication> runApplication(clazz : KClass<C>, args : Array<String>) {
     val springApplication = SpringApplication(clazz.java)
-    val commonProperties = PropertiesLoaderUtils.loadProperties(ClassPathResource("easyalpha-common-application.properties"))
+    val commonProperties = PropertiesLoaderUtils.loadProperties(ClassPathResource("easyalpha-common-application.properties", clazz.java.classLoader))
     springApplication.setDefaultProperties(commonProperties)
     springApplication.run(*args)
 }
