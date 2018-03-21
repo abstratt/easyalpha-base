0a9274f6106bb232d89c4dba6ef9d2174aedccc2
Rafael Chaves <rafael@abstratt.com>
2018-03-21 11:53:27 -0300

    Spring Boot 2.0 and small refactoring

--- easyalpha-core/src/main/java/com/abstratt/easyalpha/core/infra/EasyAlphaApplication.kt
+++ easyalpha-core/src/main/java/com/abstratt/easyalpha/core/infra/EasyAlphaApplication.kt
@@ -1,5 +1,7 @@
 package com.abstratt.easyalpha.core.infra
 
+import com.abstratt.kirra.spring.KirraJavaApplication
+import com.abstratt.kirra.spring.user.UserRole
 import org.springframework.boot.SpringApplication
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.support.PropertiesLoaderUtils
@@ -8,19 +10,7 @@ import kotlin.collections.LinkedHashMap
 import kotlin.reflect.KClass
 
 
-abstract class EasyAlphaApplication {
-    constructor() {
-        // remember this application class
-        applications[this.javaClass.`package`.name] = this.javaClass.kotlin
-    }
-    companion object {
-        private var applications : MutableMap<String, KClass<EasyAlphaApplication>> = LinkedHashMap()
-    }
+abstract class EasyAlphaApplication(userRoles : Iterable<out UserRole>) : KirraJavaApplication(userRoles) {
 }
 
-fun <C : EasyAlphaApplication> runApplication(clazz : KClass<C>, args : Array<String>) {
-    val springApplication = SpringApplication(clazz.java)
-    val commonProperties = PropertiesLoaderUtils.loadProperties(ClassPathResource("easyalpha-common-application.properties", clazz.java.classLoader))
-    springApplication.setDefaultProperties(commonProperties)
-    springApplication.run(*args)
-}
+
